96608682dee5c6be70834182ab048cf6975de369
Alejandro Quiroz <alejandro.devop@gmail.com>
2023-03-02 17:55:11 -0500

    Delete src/database directory

--- src/database/Migration.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-namespace Alejodevop\Gfox\Database;
-abstract class Migration {
-    public abstract function mount(Blueprint $table): Blueprint;
-    public abstract function unmount(Blueprint $table): Blueprint;
-}
